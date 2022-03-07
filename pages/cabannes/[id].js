e55d276f68c5a717b52d3d02ec13ac8da8c3d8b7
mrprst <prost.mathias@gmail.com>
2022-03-07 17:42:33 +0100

    pages added

---
+++ pages/cabannes/[id].js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Profile = () => {
+
+
+
+  return (
+    <div>
+      <h1>Profile!</h1>
+    </div>
+  )
+}
+
+export default Profile
---
+++ pages/cabannes/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Cabannes = () => {
+
+
+
+  return (
+    <div>
+      <h1>Cabannes!</h1>
+    </div>
+  )
+}
+
+export default Cabannes
