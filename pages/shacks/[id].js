c6942b36c4411074ce3a65949b22a18989b099f5
mrprst <prost.mathias@gmail.com>
2022-03-08 16:38:49 +0100

    fix with props and shacks name

--- pages/shacks/[id].js
+++ pages/shacks/[id].js
@@ -1,24 +1,26 @@
 import Link from "next/link";
 import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const Profile = (props) => {
+const ShackPage = ({ shack}) => {
+
   return (
     <div>
-      <h1>{cabane?.title}!</h1>
+      <h1>{shack.title}!</h1>
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
-  const cabane = await prisma.cabane.findUnique({
+  const shack = await prisma.cabane.findUnique({
     where: { id: parseInt(id) },
   });
   return {
     props: {
-      cabane,
+      shack,
     },
   };
 }
 
-export default Profile;
+export default ShackPage;
