bf41e5fb968a02e94510f2e2a4f37e015b379ecf
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-03-09 16:03:57 +0100

    footer done

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,13 @@
 import Footer from "../footer";
 import Navbar from "../navbar";
+import styles from './Layout.module.css'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={styles.layoutContainer}>
       <Navbar />
       <div className="container">{children}</div>
       <Footer />
-    </>
+    </div>
   );
 }
