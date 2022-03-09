8980de29aa260f73af1b6457f4bbe53f031326ac
VioMrqs <violette.marquis@gmail.com>
2022-03-09 10:53:05 +0100

    add authorization + see owner email

--- pages/shacks/[id].js
+++ pages/shacks/[id].js
@@ -1,40 +1,44 @@
-import Link from "next/link";
 import { PrismaClient } from "@prisma/client";
-import Image from "next/image";
+import { useReducer } from "react";
 import classes from "./shacks.module.css";
+import { useUserContext } from "../../context/UserContext";
 
 const prisma = new PrismaClient();
 
-const ShackPage = ({ shack, user }) => {
-  return (
-    <div className="mb-4 row">
-      <div className="col-10">
-        <div
-          className={classes.shackImg}
-          style={{ backgroundImage: `url(${shack?.imageUrl})` }}
-        ></div>
-        <div className="row my-2">
-          <div className="col-10">
-            <h3>{shack?.title}!</h3>
+  const ShackPage = ({ shack }) => {
+    const { user } = useUserContext();
+
+    return (
+      <div className="mb-4 row">
+        <div className="col-10">
+          <div
+            className={classes.shackImg}
+            style={{ backgroundImage: `url(${shack?.imageUrl})` }}
+          ></div>
+          <div className="row my-2">
+            <div className="col-10">
+              <h3>{shack?.title}!</h3>
+            </div>
+            <div className="col-2 text-end">
+              <span className="fs-4 fw-bold">{shack?.price} €</span>
+            </div>
           </div>
-          <div className="col-2 text-end">
-            <span className="fs-4 fw-bold">{shack?.price} €</span>
+          <div className="mb-2">
+            <span>{shack?.location}</span>
           </div>
-        </div>
-        <div className="mb-2">
-          <span>{shack?.location}</span>
-        </div>
-        <div>
-          <h3>Description du bien</h3>
-          <p>{shack?.description}</p>
+          <div>
+            <h3>Description du bien</h3>
+            <p>{shack?.description}</p>
+            {user ? (
+              <div className="col">
+                <p>Le propriétaire : {shack?.owner.email}</p>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
-      <div className="col-2">
-        <span>{user?.email}</span>
-      </div>
-    </div>
-  );
-};
+    );
+  };
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
