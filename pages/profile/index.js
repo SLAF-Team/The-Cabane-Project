d555f86988fad6617ca8e99c8f0a423b68a2296e
VioMrqs <violette.marquis@gmail.com>
2022-03-10 11:46:14 +0100

    button added + fonction to work on

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,12 +3,19 @@ import { useUserContext } from "../../context/UserContext";
 const Profile = () => {
   const { user } = useUserContext();
 
+  const handleDeleteUser =() => {
+console.log("supressionnnnn")
+// ouvrir une modal pour confirmer le choix
+// déclencher la fonction dans l'API
+  }
   return (
     <>
       {user ? (
         <div>
           <h1>{user.name}</h1>
           <h2>{user.email}</h2>
+          <button className="btn btn-secondary" onClick={() => handleDeleteUser()}>Supprimer mon profil</button>
+          <button className="btn btn-secondary">Editer mon profil</button>
         </div>
       ) : null}
     </>
